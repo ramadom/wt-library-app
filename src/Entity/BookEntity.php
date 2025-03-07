53e90c85076c37913089bf8a24ff23cecaba1c5b
A.R <ramadom.dev@gmail.com>
2025-03-07 21:43:29 +0200

    Updated database configuration and dependencies
    Added book entity, repository and migration

--- src/Entity/BookEntity.php
+++ src/Entity/BookEntity.php
@@ -2,6 +2,31 @@
 
 namespace WT\Library\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+use WT\Library\Repository\BookRepository;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+#[ORM\Table(name: "book")]
 class BookEntity {
+
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column(type: "integer")]
+  public $id;
+
+  #[ORM\Column(type: "string", length: 200)]
+  public $title;
+
+  #[ORM\Column(type: "string", length: 13)]
+  public $ISBN;
+
+  #[ORM\Column(type: "date")]
+  public $publicationDate;
+
+  #[ORM\Column(type: "string", length: 50)]
+  public $genre;
+
+  #[ORM\Column(type: "integer")]
+  public $copies;
 
 }
--- src/Repository/BookRepository.php
+++ src/Repository/BookRepository.php
@@ -3,7 +3,14 @@
 namespace WT\Library\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use WT\Library\BookEntity;
 
 class BookRepository extends ServiceEntityRepository {
+
+  #[\Override]
+  public function __construct(ManagerRegistry $registry) {
+    parent::__construct($registry, BookEntity::class);
+  }
 
 }
